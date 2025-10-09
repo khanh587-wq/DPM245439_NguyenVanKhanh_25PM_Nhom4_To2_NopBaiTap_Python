b0bca2daba180a8b578ab1fd661a36d446092ffc
khanh587-wq <khanhdz587@gmail.com>
2025-10-09 10:01:38 +0700

    9/10

--- Cguong2_Cau7.py
+++ Cguong2_Cau7.py
@@ -1,2 +1,12 @@
-biến = kiểu dữ liệu(input())
+# Nhập dữ liệu dạng chuỗi
+name = input("Nhập tên của bạn: ")
+print("Xin chào,", name)
+
+# Nhập số nguyên và chuyển kiểu
+age = int(input("Nhập tuổi của bạn: "))
+print("Tuổi của bạn là:", age)
+
+# Nhập nhiều số nguyên trên cùng một dòng
+a, b = map(int, input("Nhập 2 số nguyên cách nhau dấu cách: ").split())
+print("Hai số vừa nhập là:", a, b)
 
---
+++ Chuong2_Cau5.py
@@ -0,0 +1,22 @@
+# Đây là ghi chú dòng đơn
+
+'''
+Đây là ghi chú
+nhiều dòng sử dụng
+dấu 3 nháy đơn
+'''
+
+"""
+Đây cũng là ghi chú
+nhiều dòng sử dụng
+dấu 3 nháy kép
+"""
+
+def greet():
+    """
+    Đây là docstring - ghi chú
+    mô tả chức năng của hàm
+    """
+    print("Hello!")
+
+greet()
